f680d041c59222d0b38b008fdb43dc0813291eec
Ethan Merrill <eth22mer@gmail.com>
2020-10-23 17:59:49 -0400

    Polygon Api For initial Universe (its a mess)

--- fastquant3.py
+++ fastquant3.py
@@ -10,8 +10,13 @@ import json
 import pandas as pd
 import time
 from datetime import date
+# import polygon
+# from polygon import RESTClient"
+
 # Set Finnhub api keys
 finnhubKey = keys.keys.get("finnhub")
+alpacaKey = keys.keys.get("alpaca")
+IEXKey = keys.keys.get("iex")
 
 r = requests.get(f'https://finnhub.io/api/v1/stock/symbol?exchange=US&token={finnhubKey}')
 
@@ -55,14 +60,32 @@ allTickers.to_pickle(f"Stock_universe_{date.today()}")
 # Depickle and filter by market cap larger than x
 allTickers = pd.read_pickle(f"Stock_universe_{date.today()}")
 
-allTickers = allTickers.loc[allTickers['mktCap']>=300 ]
+allTickers = allTickers.loc[allTickers['mktCap']>=600 ]
 
 
 #%%
+def get_Volume_Price(symbol):
+    attempts = 0
+    while attempts<5:
+        try:
+            symbol = yf.Ticker(symbol)
+        try:
+            # time.sleep(1)
+            
+            volume = (symbol.info.get("averageVolume10days"))
+            closePrice = (symbol.info.get("previousClose"))
+            print(f"Symbol: {symbol} volume: {volume} closePrice:{closePrice}")
+            break
 
+        except Exception as e, err:
+            attempts += 1
+            print(f"error in Volume_Price {e} {symbol} ERR: {err} ")
 
-msft = yf.Ticker("MSFT")
-msft.info
+    return volume, closePrice 
+
+#%%
+allTickers["averageVolume10days"],allTickers["closePrice"] = allTickers.symbol.apply(get_Volume_Price)
+
 #%%
 
 df = get_stock_data("aapl", "2018-01-01", "2020-10-01")
@@ -72,7 +95,43 @@ print(df.head())
 backtest('smac', df, fast_period=10, slow_period=40,init_cash=1000)
 
 # %%
+polygonData = requests.get(f"https://api.polygon.io/v2/reference/tickers?apiKey={alpacaKey}").json()
+
+# %%
+polygonTickersDataFrame = pd.DataFrame()
+
+errors=0
+if errors<2:
+    pageNumber = 0
+    # ugly and bad:
+    polygonTickersData = requests.get(f"https://api.polygon.io/v2/reference/tickers?sort=ticker&type=cs&market=STOCKS&locale=us&perpage=50&page={pageNumber}&active=true&apiKey={alpacaKey}").json().get("tickers")
+
+    while (polygonTickersData != []):
+        try:
+            polygonTickersData = requests.get(f"https://api.polygon.io/v2/reference/tickers?sort=ticker&type=cs&market=STOCKS&locale=us&perpage=50&page={pageNumber}&active=true&apiKey={alpacaKey}").json().get("tickers")
+            pageNumber += 1
+            polygonTickersDataFrame = polygonTickersDataFrame.append(polygonTickersData)
+            print(f"working on page Number: {pageNumber}")
+        except Exception as e:
+            errors += 1
+            print(f"error in Volume_Price {e}  ")
+
+
+#%%
+# Filter out OTC things
+polygonTickersDataFrame = polygonTickersDataFrame.loc[polygonTickersDataFrame["primaryExch"]!="OTC"]
 
 
 
 # %%
+polygonTickersData = requests.get(f"https://api.polygon.io/v2/aggs/grouped/locale/US/market/STOCKS/{date.today()}?unadjusted=false&apiKey={alpacaKey}").json().get("results")# %%
+
+# %%
+PolygonTickersDataFrame = pd.DataFrame(polygonTickersData)
+# Filter 
+# %%
+# Volume over 1mil, close price over 15&under 200
+PolygonTickersDataFrame= PolygonTickersDataFrame.loc[(PolygonTickersDataFrame["v"]>=1000000 ) & (PolygonTickersDataFrame["c"]>=15 ) & (PolygonTickersDataFrame["c"]<=200)]
+
+# %%
+PolygonTickersDataFrame["mktCap"] = PolygonTickersDataFrame.T.apply(get_MKT_Cap)
