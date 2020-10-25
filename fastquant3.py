85ea3ccc7e1d22391eea78db09397b38febd9b82
Ethan Merrill <eth22mer@gmail.com>
2020-10-24 22:38:14 -0400

    Working roi&buy&hold columns

--- fastquant3.py
+++ fastquant3.py
@@ -9,7 +9,7 @@ import keys
 import json
 import pandas as pd
 from time import time
-from datetime import date
+from datetime import datetime, timedelta, date
 import numpy as np
 # import polygon
 # from polygon import RESTClient"
@@ -53,16 +53,24 @@ def get_All_Tickers(date = (date.today())):
 # backtest('smac', df, fast_period=10, slow_period=40,init_cash=1000)
 # %%
 
-def rsi_optimizer(periods_list, rsi_lower_list, rsi_upper_list, symbol, start_date, end_date=str(date.today()), init_cash=1000):
+def rsi_optimizer(periods_list, rsi_lower_list, rsi_upper_list, symbol, start_date, end_date=date.today(), init_cash=1000):
     # Start the timer!
     start_time = time()
+    # overly complex date handling system to add one day to end date:
+    if (type(end_date)==str):
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    end_date = end_date+timedelta(days=1)
+    end_date = datetime.strftime(end_date, "%Y-%m-%d")
 
+    # 
     null_return_dict = {
     "symbol":symbol,
     "optimal_rsi_period" : None,
     "optimal_rsi_lower" : None,
     "optimal_rsi_upper" : None,
-    "profit": None}
+    "profit": None,
+    "roi":None,
+    "buy_and_hold":None}
 
     #Save all the inputs for recreation/debuggin/testing
     # input_conditions = locals()
@@ -111,13 +119,22 @@ def rsi_optimizer(periods_list, rsi_lower_list, rsi_upper_list, symbol, start_da
     optimal_rsi_lower = rsi_lower_list[best_params_position[1]]
     optimal_rsi_upper = rsi_upper_list[best_params_position[2]]
     profit = np.amax(period_grid)
+
+    #calculate the percent return of the strategy (ROI)
+    roi = profit/init_cash
+
+    # calculate change in the equity over the time period
+    buy_and_hold = (stock_data_df["close"].iloc[-1]-stock_data_df["close"].iloc[0])/stock_data_df["close"].iloc[0]
+
     # make a dict with the values to return
     return_dict = {
         "symbol":symbol,
         "optimal_rsi_period" : optimal_rsi_period,
         "optimal_rsi_lower" : optimal_rsi_lower,
         "optimal_rsi_upper" : optimal_rsi_upper,
-        "profit":profit 
+        "profit":profit,
+        "roi": roi,
+        "buy_and_hold": buy_and_hold
     }
 
     # Calculate total time taken by the function
@@ -149,36 +166,39 @@ def rsi_optimizer(periods_list, rsi_lower_list, rsi_upper_list, symbol, start_da
 def multi_stock_rsi_optimize(df_of_stocks):
     start_time = time()
     # Add empty columns to dataframe
-    results_df = pd.DataFrame(columns = ["symbol", "optimal_rsi_period","optimal_rsi_lower","optimal_rsi_upper","profit"])
+    results_df = pd.DataFrame(columns = ["symbol", "optimal_rsi_period","optimal_rsi_lower","optimal_rsi_upper","profit", "roi", "buy_and_hold"])
     symbol_count=0
     for symbol in df_of_stocks["T"]:
 
         symbol_dict = rsi_optimizer([3,18,4], [24,35,5],[64,75,5], symbol, "2020-01-01", "2020-10-23")
         results_df = results_df.append(symbol_dict, ignore_index=True)
+        # Temp save function to salvage some data from a very long test
+        if (symbol_count % 3 == 0):
+            newDf.to_pickle(f"Partial_Backtest_Save_{date}")
         symbol_count=+1
+
 
     end_time = time()
     time_basic = end_time-start_time
     return results_df, time_basic
 
 #%%
-# all_ticks = get_All_Tickers("2020-10-23").loc[0:100]
+all_ticks = get_All_Tickers("2020-10-23").loc[0:3]
 # if all_ticks.empty==True :
 #     print("could not find ticks")
-# newDf, time_basic = multi_stock_rsi_optimize(all_ticks)
+newDf, time_basic = multi_stock_rsi_optimize(all_ticks)
+newDf.to_pickle(f"Full_Backtest_{date}")
 # print(newDf,time_basic)
-# %%
-stock_data_df = get_stock_data("nvda", "2020-01-01", "2020-10-2")
+# # %%
+# stock_data_df = get_stock_data("nvda", "2020-01-01", "2020-10-27")
 
-# %%
-print(f'''
-startprice:{stock_data_df["close"].iloc[0]} 
-enddate:{stock_data_df["close"].iloc[-1]}
-
-
-''')
-
-print((stock_data_df["close"].iloc[-1]-stock_data_df["close"].iloc[0])/stock_data_df["close"].iloc[0])
+# # %%
+# print(f'''
+# startprice:{stock_data_df["close"].iloc[0]} 
+# enddate:{stock_data_df["close"].iloc[-1]}
 
 
-# %%
+# ''')
+
+# print((stock_data_df["close"].iloc[-1]-stock_data_df["close"].iloc[0])/stock_data_df["close"].iloc[0])
+
--- readme.md
+++ readme.md
@@ -3,6 +3,7 @@
 Some Experimenting with a trading algorithm for US Common Stock. 
 
 todo:
+- [ ] Add a higher set of variables such as dates in which the backtest is run (currently hardcoded in multi_rsi_opti funct), save directories, 
 - [ ] Add a better time estimator, this will be useful as we move to backtesting multiple equities at once. 
 - [ ] Remove all dependency on yfinance
 - [ ] Make a automatic date getter (get most recent weekday, if no today)
