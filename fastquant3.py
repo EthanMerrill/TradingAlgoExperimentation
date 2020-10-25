7ace89a6addc5553c89b1b8eb83620064f730ae2
Ethan Merrill <eth22mer@gmail.com>
2020-10-24 21:11:17 -0400

    More Robust pickling, error catching.

--- fastquant3.py
+++ fastquant3.py
@@ -19,10 +19,23 @@ import numpy as np
 alpacaKey = keys.keys.get("alpaca")
 # IEXKey = keys.keys.get("iex")
 
+# MIsc global options
+pd.set_option('display.max_rows', None)
 
 
 # %%
 def get_All_Tickers(date = (date.today())):
+
+    #chech to see if a pickled version for requested date exists, if so, just return that:
+    try:
+        # SAVE THE DF Locally because this operation uses a lot of api pts wherever you do it. 
+        polygon_tickers_dataframe = pd.read_pickle(f"Stock_universe_{date}")
+        print("existing Universe for selected date found, loaded cache")
+        return polygon_tickers_dataframe
+    except:
+        print(f"existing universe for {date} not found, querying polygon API")
+        pass
+
     # function to find & filter all symbols for any date
     # #Get all tickers:
     polygonTickersData = requests.get(f"https://api.polygon.io/v2/aggs/grouped/locale/US/market/STOCKS/{str(date)}?unadjusted=false&apiKey={alpacaKey}").json().get("results")
@@ -31,7 +44,9 @@ def get_All_Tickers(date = (date.today())):
     # Filter 
     # Volume over 1mil, close price over 15&under 200
     polygon_tickers_dataframe = polygon_tickers_dataframe.loc[(polygon_tickers_dataframe["v"]>=1000000 ) & (polygon_tickers_dataframe["c"]>=15 ) & (polygon_tickers_dataframe["c"]<=200)]
-
+    polygon_tickers_dataframe.sort_values(by=['T'])
+    polygon_tickers_dataframe.reset_index(inplace = True)
+    polygon_tickers_dataframe.to_pickle(f"Stock_universe_{date}")
     return polygon_tickers_dataframe
 
 # someObject = backtest('rsi', df, rsi_period=14, rsi_upper=70, rsi_lower=30, init_cash=1000)
@@ -41,6 +56,14 @@ def get_All_Tickers(date = (date.today())):
 def rsi_optimizer(periods_list, rsi_lower_list, rsi_upper_list, symbol, start_date, end_date=str(date.today()), init_cash=1000):
     # Start the timer!
     start_time = time()
+
+    null_return_dict = {
+    "symbol":symbol,
+    "optimal_rsi_period" : None,
+    "optimal_rsi_lower" : None,
+    "optimal_rsi_upper" : None,
+    "profit": None}
+
     #Save all the inputs for recreation/debuggin/testing
     # input_conditions = locals()
     # get historical stock data (may need to update this to use polygon, not Yfinance)
@@ -48,8 +71,9 @@ def rsi_optimizer(periods_list, rsi_lower_list, rsi_upper_list, symbol, start_da
     stock_data_df = get_stock_data(symbol, str(start_date), str(end_date))
     if stock_data_df.empty == True:
         print(f"unable to find yahoo data for {symbol}")
-        return [symbol, "null", "null", "null"]   
-    
+        return null_return_dict
+
+    # print(stock_data_df)
     # make a list of all the values to test
     periods_list = np.arange(periods_list[0],periods_list[1],periods_list[2], dtype=int)
     rsi_lower_list = np.arange(rsi_lower_list[0],rsi_lower_list[1],rsi_lower_list[2], dtype=int)
@@ -77,7 +101,9 @@ def rsi_optimizer(periods_list, rsi_lower_list, rsi_upper_list, symbol, start_da
                     period_grid[i,j,k] = net_profit
     except Exception as e:
         print(f"error occured in rsi_optimizer: {e}")     
-        return [symbol, "null", "null", "null"]   
+
+        return null_return_dict
+  
     # Find highest profit
     best_params_position = np.unravel_index(period_grid.argmax(), period_grid.shape)
     # find position of highest profit
@@ -91,7 +117,7 @@ def rsi_optimizer(periods_list, rsi_lower_list, rsi_upper_list, symbol, start_da
         "optimal_rsi_period" : optimal_rsi_period,
         "optimal_rsi_lower" : optimal_rsi_lower,
         "optimal_rsi_upper" : optimal_rsi_upper,
-        "profit":profit
+        "profit":profit 
     }
 
     # Calculate total time taken by the function
@@ -127,7 +153,7 @@ def multi_stock_rsi_optimize(df_of_stocks):
     symbol_count=0
     for symbol in df_of_stocks["T"]:
 
-        symbol_dict = rsi_optimizer([3,18,5], [25,35,5],[65,75,5], symbol, "2020-01-01", "2020-10-23")
+        symbol_dict = rsi_optimizer([3,18,4], [24,35,5],[64,75,5], symbol, "2020-01-01", "2020-10-23")
         results_df = results_df.append(symbol_dict, ignore_index=True)
         symbol_count=+1
 
@@ -136,9 +162,23 @@ def multi_stock_rsi_optimize(df_of_stocks):
     return results_df, time_basic
 
 #%%
-all_ticks = get_All_Tickers("2020-10-23").loc[0:10]
-if all_ticks.empty==True :
-    print("could not find ticks")
-newDf, time_basic = multi_stock_rsi_optimize(all_ticks)
-print(newDf,time_basic)
+# all_ticks = get_All_Tickers("2020-10-23").loc[0:100]
+# if all_ticks.empty==True :
+#     print("could not find ticks")
+# newDf, time_basic = multi_stock_rsi_optimize(all_ticks)
+# print(newDf,time_basic)
+# %%
+stock_data_df = get_stock_data("nvda", "2020-01-01", "2020-10-2")
+
+# %%
+print(f'''
+startprice:{stock_data_df["close"].iloc[0]} 
+enddate:{stock_data_df["close"].iloc[-1]}
+
+
+''')
+
+print((stock_data_df["close"].iloc[-1]-stock_data_df["close"].iloc[0])/stock_data_df["close"].iloc[0])
+
+
 # %%
